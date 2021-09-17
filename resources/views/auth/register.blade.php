9d438d5f73bbabe93973bb9975a82d1736a55a6a
Krybe12 <Krybe120@gmail.com>
2021-09-17 10:16:32 +0200

    fix user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,5 +2,33 @@
 @section('title') Register @endsection
 
 @section('content')
-  <h1>ahoj register</h1>
+  <div class="container-fluid h-100 bg-secondary">
+    <div class="d-flex justify-content-center py-5">
+      <div class="bg-light rounded-3">
+        <form action="" method="POST" class="p-4">
+          @csrf
+          <div class="mb-3">
+            <label  class="form-label">Username</label>
+            <input type="text" name="name" class="form-control" placeholder="John doe">
+          </div>
+          <div class="mb-3">
+            <label  class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" placeholder="">
+          </div>
+          <div class="mb-3">
+            <label  class="form-label">Repeat Password</label>
+            <input type="password" name="password_confirmation" class="form-control" placeholder="">
+          </div>
+          <div class="mb-3">
+            <input type="submit" class="form-control" placeholder="">
+          </div>
+        </form>
+      </div>
+      @if ($errors->any())
+      @foreach ($errors->all() as $error)
+          {{$error}}
+      @endforeach
+    @endif
+    </div>
+  </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,7 +19,7 @@
     }
   </style>
 </head>
-<body>
+<body class="h-100">
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
