a1a286c479b9221326a00ce0b98d8fcfad00f346
Krybe12 <Krybe120@gmail.com>
2021-09-20 14:13:19 +0200

    add state table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,30 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use App\Models\State;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $arr = ["Factory New", "Minimal Wear", "Field-Testes", "Well-Worn", "Scrap"];
+    foreach ($arr as $i) {
+      $state = new State;
+      $state->name = $i;
+      $state->save();
     }
+    // \App\Models\User::factory(10)->create();
+    /*         DB::table('states')->insert(['name' => "Nový"]);
+        DB::table('states')->insert(['name' => "Lehce opotřebený"]);
+        DB::table('states')->insert(['name' => "Opotřebný"]);
+        DB::table('states')->insert(['name' => "Velmi opotřebený"]);
+        DB::table('states')->insert(['name' => "Na součástky"]); */
+  }
 }
