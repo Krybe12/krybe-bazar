b2c821941d1694c65fa6fbcba55e3dbce9bb0674
Krybe12 <Krybe120@gmail.com>
2021-09-24 09:43:33 +0200

    .

--- resources/views/addlisting.blade.php
+++ resources/views/addlisting.blade.php
@@ -48,7 +48,7 @@
               <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
               <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
             </svg> Upload main image</label>
-            <input type="file" class="form-control" accept="image/png, image/jpeg, image/heic" name="mainimg" required>
+            <input type="file" class="form-control" accept="image/png, image/jpeg, image/heic" name="image" required>
           </div>
           <div class="mb-3">
             <label  class="form-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image-fill" viewBox="0 0 16 16">
