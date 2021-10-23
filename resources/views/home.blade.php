4a058953342163ea9207f4d0dfa7b51e635c43f3
Krybe12 <Krybe120@gmail.com>
2021-10-24 00:26:48 +0200

    css change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,14 @@
     <div class="d-flex justify-content-md-start justify-content-center ps-0 ps-md-4  align-items-center pt-4">
       <h2>Categories</h2>
     </div>
-    <div class="d-flex flex-row flex-md-column p-0 p-md-3 flex-wrap text-center text-md-start justify-content-evenly justify-content-md-left">
+    <div class="d-flex flex-row flex-md-column p-0 p-md-3 flex-wrap text-center text-md-start justify-content-center">
       @foreach ($categories as $category)
-        <div class="d-flex ms-md-3 mb-1 p-1 rounded-3 flex-column flex-md-row ct">
+      <a  href="">
+        <div class="d-flex ms-md-3 p-1 rounded-3 flex-column flex-md-row ct align-items-center">
           <i class="bi bi-{{ $category->icon }}"></i>
-          <a class="ps-1 flex-fill" href="">{{ $category->name }}</a>
+          <p class="ps-1 my-2"> {{ $category->name }}</p>
         </div>
+        </a>
       @endforeach
     </div>
   </div>
