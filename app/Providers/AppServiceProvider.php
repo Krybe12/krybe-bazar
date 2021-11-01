53d51a38b0752b9a2ae83ce70aa7e12f45ab2e02
Krybe12 <Krybe120@gmail.com>
2021-11-01 14:40:11 +0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
         if($this->app->environment('production')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
