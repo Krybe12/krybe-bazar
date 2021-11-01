279e67d0495bcc1b13fbcffa897560f2dd085cd8
Krybe12 <Krybe120@gmail.com>
2021-11-01 17:31:53 +0100

    https fix try

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+if (env('APP_ENV') == 'production') {
+  URL::forceScheme('https');
+}
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
