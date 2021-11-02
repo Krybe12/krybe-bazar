4600290717eaa4ca0d7c2093aee77d58f5fae225
Krybe12 <Krybe120@gmail.com>
2021-11-02 09:37:02 +0100

    replace category tags to buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,20 @@
 
 @section('content')
 <div class="d-flex flex-column flex-md-row pt-4 px-0 px-md-2 px-lg-4">
-  <div class="col-auto bg-white rounded-3 shadow" style="max-height: 350px;">
+  <div class="col-auto bg-white rounded-3 shadow">
     <div class="d-flex justify-content-md-start justify-content-center ps-1 ps-md-3 pt-2 pb-0 align-items-center">
       <h2>Categories</h2>
     </div>
     <div class="d-flex flex-row flex-md-column p-0 p-md-3 flex-wrap text-center text-md-start justify-content-center">
       
       @foreach ($categories as $category)
-        <a  href="/category/{{ $category->name }}">
-          <div class="d-flex ms-md-0 p-1 rounded-3 flex-column flex-md-row ct align-items-center">
-            <i class="bi bi-{{ $category->icon }}"></i>
-            <p class="ps-1 my-1"> {{ $category->name }}</p>
+          <div class="d-flex ms-md-0 p-1 rounded-3 flex-column flex-md-row ct">
+            <button type="button" class="btn d-flex w-100 align-items-center">
+              <i class="bi bi-{{ $category->icon }}"></i>
+              <p class="ps-1 my-1"> {{ $category->name }}</p>
+            </button>
+
           </div>
-        </a>
       @endforeach
 
     </div>
@@ -32,11 +33,11 @@
 <script>
 class Offers{
   constructor(){
-    this.page = 1;
     this.perPage = 5;
+    this.category = "";
     this.e = document.getElementById("mp")
   }
-  async setEventListeners(){
+  async setPaginationEventListeners(){
     document.querySelectorAll('.pagination a').forEach(el => {
       el.addEventListener("click", (e) => {
         e.preventDefault();
@@ -48,7 +49,7 @@ class Offers{
     await fetch(`/offers/?page=${page}`)
       .then(response => response.text())
       .then(data => this.e.innerHTML = data);
-    await this.setEventListeners();
+    await this.setPaginationEventListeners();
   }
 }
 let page = new Offers()
