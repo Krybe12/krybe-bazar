a09a8d3dd11b1101f4e454be9b5dbb0f88615fa6
Krybe12 <Krybe120@gmail.com>
2021-12-01 14:39:29 +0100

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,5 +6,9 @@
     <div class="col">
       <h1 class="pb-md-4 pb-2"><b> Profile of {{ $user->user_name }} </b></h1>
     </div>
+
+    <div class="col d-flex flex-column flex-md-row border shadow bg-white rounded-3 p-2">
+      asd
+    </div>
   </div>
 @endsection
