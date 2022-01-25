e2772184d05d624e64783c5f6eb50c8b362ddebd
Krybe12 <Krybe120@gmail.com>
2022-01-25 11:48:40 +0100

    code cleanup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,4 @@ class HomeController extends Controller
   public function index(){
     return view('home', ['categories' => Category::all(), 'offers' => Offer::paginate(5)]);
   }
-  public function category($catName){
-    dd(Category::where('name', $catName)->first()->id);
-  }
 }
