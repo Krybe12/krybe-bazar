ba3dfe1473fff18f72ba033539f78f8e642d9dd3
Krybe12 <Krybe120@gmail.com>
2022-01-27 08:14:01 +0100

    add stats page

---
+++ resources/views/stats.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('title') Stats @endsection
+
+@section('content')
+    <div class="d-flex flex-column align-items-center justify-content-center py-4">
+      <div>
+        <h1 class="pb-md-5 pb-2"><b>Stats</b></h1>
+      </div>
+      asd
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ListingController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\InformationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StatsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/stats', [StatsController::class, 'index']);
 
 Route::get('/offers', [OfferController::class, 'getOffers']);
 Route::get('/offer/{offerTag}', [OfferController::class, 'getOffer']);
