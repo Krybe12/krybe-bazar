44af8b46b37169ea76757f6e4b5e2ef3970cdce9
Krybe12 <Krybe120@gmail.com>
2022-02-11 08:34:44 +0100

    add stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use App\Models\Currency;
+use App\Models\Category;
+use App\Models\Offer;
+use App\Models\File;
+use App\Models\State;
+use App\Models\User;
 
 class StatsController extends Controller
 {
   public function index()
   {
-    return view('stats');
+    $dataArr = [];
+    array_push($dataArr,
+    ["name" => "Registered users", "data" => User::count()],
+    ["name" => "Number of offers", "data" => Offer::count()],
+    ["name" => "Images uploaded", "data" => File::count()],
+    ["name" => "Number of admins", "data" => Admin::count()],
+    ["name" => "Number of categories", "data" => Category::count()],  
+    );
+    return view('stats', ["dataArr" => $dataArr]);
   }
 }
--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -6,6 +6,18 @@
       <div>
         <h1 class="pb-md-5 pb-2"><b>Stats</b></h1>
       </div>
-      asd
+      <div class="row">
+        @foreach ($dataArr as $data)
+
+          <div class="col d-flex flex-column align-items-center text-center p-4" style="max-width: 250px">
+            <h2> {{ $data["data"] }} </h2>
+            <hr class="border border-dark bg-dark mt-0 w-100">
+            <h4> {{ $data["name"] }} </h4>
+          </div>
+
+        @endforeach
+            
+      </div>
+  
     </div>
 @endsection
